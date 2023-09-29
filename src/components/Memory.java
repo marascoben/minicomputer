5f56cf4847069ce9aa64cbd585f2df7667231fc0
Benjamin Marasco <marascoben@gmail.com>
2023-09-29 10:57:43 -0400

    Implement processor and memory

--- src/components/Memory.java
+++ src/components/Memory.java
@@ -4,9 +4,29 @@ import java.util.Map;
 
 public class Memory {
 
+    // The available memory size
+    public static final int MEMORY_SIZE = 2048;
+
     // Memory region
     private Map<Character, Character> region;
 
     public Memory() {
+        region = new java.util.HashMap<Character, Character>();
+    }
+
+    public char Read(char address) {
+        if (address < 0 || address >= MEMORY_SIZE) {
+            throw new IllegalArgumentException("Invalid memory address");
+        }
+
+        return region.getOrDefault(address, (char) 0);
+    }
+
+    public void Write(char address, char data) {
+        if (address < 0 || address >= MEMORY_SIZE) {
+            throw new IllegalArgumentException("Invalid memory address");
+        }
+
+        region.put(address, data);
     }
 }
--- src/components/Processor.java
+++ src/components/Processor.java
@@ -3,24 +3,31 @@ package components;
 public class Processor {
 
     // General Purpose Registers
-    private char R0 = 0, R1 = 0, R2 = 0, R3 = 0;
+    public char R0 = 0, R1 = 0, R2 = 0, R3 = 0;
 
     // Index Registers
-    private char X1 = 0, X2 = 0, X3 = 0;
+    public char X1 = 0, X2 = 0, X3 = 0;
 
-    // Memory Address Register, Memory Buffer Register
-    private char MAR = 0, MBR = 0;
+    // Memory Address, Memory Buffer Registers
+    public char MAR = 0, MBR = 0;
+
+    // Program Counter
+    public char PC = 0;
 
     // Instruction Register
     private char IR = 0;
 
-    // Program Counter
-    private char PC = 0;
+    // Memory Fault Register
+    private byte MFR = 0;
 
     // Condition Code
     private byte CC = 0;
 
-    public Processor() {
+    // Memory Object
+    private Memory memory;
 
+    public Processor(Memory memory) {
+        this.memory = memory;
     }
+
 }
